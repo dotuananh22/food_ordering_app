0f4f0c59065aaa5eefcfe6be8fea11a315ce8d04
dotuananh <dotuananh220201@gmail.com>
2022-12-05 22:55:36 +0700

    Add converter

---
+++ food_ordering_app/food_ordering_app/Helpers/AddressConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace food_ordering_app.Helpers
+{
+    class AddressConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo)
+        {
+            return "Địa chỉ: " + value as string;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as string;
+        }
+    }
+}
