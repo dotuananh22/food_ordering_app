c2bf8f8030754b92cd6c3499987c643ed4bdee17
VoHongHoa <19521522@gm.uit.edu.vn>
2022-12-08 08:28:29 +0700

    cart

--- food_ordering_app/food_ordering_app/Views/CheckoutPage.xaml.cs
+++ food_ordering_app/food_ordering_app/Views/CheckoutPage.xaml.cs
@@ -3,28 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Essentials;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace food_ordering_app.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CartView : ContentPage
+    public partial class CheckoutPage : ContentPage
     {
-        public CartView()
+        public CheckoutPage()
         {
             InitializeComponent();
         }
 
-        private async void ImageButton_Clicked(object sender, EventArgs e)
+        private  async void ImageButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
-        }
-
-        private async void btnCheckout_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new CheckoutPage());
         }
     }
 }
