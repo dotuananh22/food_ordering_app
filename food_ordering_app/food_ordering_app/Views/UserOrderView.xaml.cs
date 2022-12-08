8ffe64f5a733569583098075133fc8bc6dc9ef69
dotuananh <dotuananh220201@gmail.com>
2022-12-08 13:40:38 +0700

    Add user order

---
+++ food_ordering_app/food_ordering_app/Views/UserOrderView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace food_ordering_app.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserOrderView : ContentPage
+    {
+        public UserOrderView()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
