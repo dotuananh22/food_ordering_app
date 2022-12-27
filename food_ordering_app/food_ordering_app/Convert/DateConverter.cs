693117dffd849b333829ffe268ccb96c42d87a29
dotuananh <dotuananh220201@gmail.com>
2022-12-27 09:21:44 +0700

    Update ui

---
+++ food_ordering_app/food_ordering_app/Convert/DateConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace food_ordering_app.Convert
+{
+    class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            var datetime = (DateTime)value;
+            return datetime.ToString("dddd, dd MMMM yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
